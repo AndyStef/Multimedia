4921aaeb627f9b1df489537736176029c7d78740
Andy <Andy.Stef.2512@gmail.com>
2017-02-16 09:01:14 +0200

    Initial commit

---
+++ Лаби/1 лаба/GLL/GLL/SharpGLForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GLL
+{
+    partial class SharpGLForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl.DrawFPS = true;
+            this.openGLControl.Location = new System.Drawing.Point(0, 0);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.FBO;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(624, 391);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(523, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Прискоритись";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(424, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Заповільнити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SharpGLForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 391);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "SharpGLForm";
+            this.Text = "SharpGL Form";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Лаби/1 лаба/GLL/GLL/SharpGLForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace GLL
+{
+    /// <summary>
+    /// The main form class.
+    /// </summary>
+    public partial class SharpGLForm : Form
+    {
+        public SharpGLForm()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        const float clockRadius = 80.0f;
+        const float clockVolume = 100.0f;
+        const float angleMin = (float)Math.PI / 30.0f;
+        const float minStart = 0.9f;
+        const float minEnd = 1.0f;
+        const float stepStart = 0.8f;
+        const float stepEnd = 1.0f;
+
+        float hourAngle = 0;
+        float minAngle = 0;
+        float secAngle = 0;
+
+        void newLine(float rStart, float rEnd, float angle, OpenGL gl)
+        {
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+            gl.Vertex(clockRadius * rStart * cos, clockRadius * rStart * sin);
+            gl.Vertex(clockRadius * rEnd * cos, clockRadius * rEnd * sin);
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs e)
+        {
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Load the identity matrix.
+            gl.LoadIdentity();
+
+            
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.LineWidth(2.0f);
+            gl.Enable(OpenGL.GL_LINE_SMOOTH);
+            gl.Enable(OpenGL.GL_POINT_SMOOTH);
+            gl.Enable(OpenGL.GL_POLYGON_SMOOTH);
+
+            //Побудова циферблату годинника
+            gl.Begin(OpenGL.GL_LINES);
+            for (int i = 0; i < 60; i++)
+            {
+                if (i % 5 != 0)
+                {
+                    if (i % 5 == 1)
+                        gl.Color(1.0f, 1.0f, 1.0f);
+                    newLine(minStart, minEnd, i * angleMin, gl);
+                }
+                else
+                {
+                    gl.Color(1.0f, 0.0f, 0.0f);
+                    newLine(stepStart, stepEnd, i * angleMin, gl);
+                }
+            }
+            gl.End();
+
+            // Малюємо стрілки
+            gl.LineWidth(3.0f);
+            gl.Begin(OpenGL.GL_LINES);
+            newLine(0.0f, 0.5f, -hourAngle + (float)Math.PI / 2, gl);
+            newLine(0.0f, 0.8f, -minAngle + (float)Math.PI / 2, gl);
+            gl.End();
+
+            //секундна
+            gl.LineWidth(1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Begin(OpenGL.GL_LINES);
+            newLine(0.0f, 0.8f, -secAngle + (float)Math.PI / 2, gl);
+            gl.End();
+           
+        }
+
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            //  TODO: Initialise OpenGL here.
+
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Set the clear color.
+            gl.ClearColor(0, 0, 0, 0);
+        }
+
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            //  TODO: Set the projection matrix here.
+            float aspectRatio;
+
+            if (Height == 0)
+                Height = 1;
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            gl.Viewport(0, 0, Width, Height);
+
+
+            //  Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            //  Load the identity.
+            gl.LoadIdentity();
+
+            aspectRatio = Width / Height;
+
+            if (Width <= Height)
+                gl.Ortho(-clockVolume, clockVolume, -clockVolume / aspectRatio, clockVolume / aspectRatio, 1.0, -1.0);
+            else
+                gl.Ortho(-clockVolume * aspectRatio, clockVolume * aspectRatio, -clockVolume, clockVolume, 1.0, -1.0);
+
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.LoadIdentity();       
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+            //secAngle -= 12.0f;
+            secAngle += speedRate;
+            minAngle += speedRate / 60;
+            hourAngle += speedRate / 360;
+        }
+
+        private float speedRate = 0.1f;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            speedRate += 0.1f;
+           // timer1.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (speedRate != 0.1f)
+            {
+                speedRate -= 0.1f;
+            }
+        }
+    }
+}
